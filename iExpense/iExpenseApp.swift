2dcf798d569d951cd0b810b3d6a83179328bc80c
pcrow101 <34235799+pcrow101@users.noreply.github.com>
2022-07-24 16:15:19 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by paul crow on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
